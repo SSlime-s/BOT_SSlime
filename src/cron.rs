f87b544bdd15c57e081953f362ccd1c1790af229
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-07-27 04:21:09 +0900

    🐛 cron の中で新しいスケジュールは作れないっぽい？ので sleep で待つようにした
    実装がわかんないけど、もしかしたら thread::spawn したほうがいいかも

--- src/cron.rs
+++ src/cron.rs
@@ -1,6 +1,6 @@
-use std::time::Duration;
+use std::{thread, time::Duration};
 
-use log::{error, debug};
+use log::{debug, error};
 use rand::Rng;
 use sqlx::MySqlPool;
 use tokio_cron_scheduler::{Job, JobScheduler};
@@ -13,45 +13,13 @@ pub async fn start_scheduling(
 ) -> anyhow::Result<tokio::task::JoinHandle<()>> {
     let main_scheduler = JobScheduler::new()?;
 
-    let create_post_job = Job::new_async("0 0,20,40 0,7-23 * * *", move |_uuid, _lock| {
+    let post_job = Job::new_async("0 0,20,40 0,7-23 * * *", move |_uuid, _lock| {
         Box::pin(async {
             let next_span = rand::thread_rng().gen_range(1..20);
-            let next_job =
-                Job::new_one_shot_async(Duration::from_secs(next_span * 60), |_uuid, _lock| {
-                    Box::pin(async {
-                        let msg = generate_message();
-                        let res = api::post_message(channel_id.to_string(), msg).await;
-                        if let Err(e) = res {
-                            error!("{}", e);
-                        }
-                    })
-                });
-            let next_job = match next_job {
-                Ok(job) => job,
-                Err(e) => {
-                    error!("{}", e);
-                    return;
-                }
-            };
-            let post_scheduler = match JobScheduler::new() {
-                Ok(scheduler) => scheduler,
-                Err(e) => {
-                    error!("{}", e);
-                    return;
-                }
-            };
-            if let Err(e) = (&post_scheduler).add(next_job) {
-                error!("{}", e);
-            }
-            let post_loop = match post_scheduler.start() {
-                Ok(loop_) => loop_,
-                Err(e) => {
-                    error!("{}", e);
-                    return;
-                }
-            };
             debug!("scheduled at {} minutes later", next_span);
-            if let Err(e) = post_loop.await {
+            thread::sleep(Duration::from_secs(next_span * 60));
+            let message = generate_message();
+            if let Err(e) = api::post_message(channel_id.to_string(), message).await {
                 error!("{}", e);
             }
         })
@@ -66,7 +34,7 @@ pub async fn start_scheduling(
         })
     })?;
 
-    main_scheduler.add(create_post_job)?;
+    main_scheduler.add(post_job)?;
     main_scheduler.add(update_markov_job)?;
 
     Ok(main_scheduler.start()?)
