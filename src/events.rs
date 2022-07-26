64987f9b8a4fc4bae6652437ece79672f9c6de47
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-07-26 13:10:27 +0900

    🐛 MENTION は別に MENTION 用のやつが作成されるわけではないっぽい

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,7 @@
 use serde_json::Value;
 
+use crate::BOT_ID;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Events {
     Ping,
@@ -9,28 +11,40 @@ pub enum Events {
 }
 impl Events {
     pub fn from_str(event_str: impl AsRef<str>) -> Result<Events, String> {
-        let event_json: Value = serde_json::from_str(event_str.as_ref()).map_err(|e| e.to_string())?;
+        let event_json: Value =
+            serde_json::from_str(event_str.as_ref()).map_err(|e| e.to_string())?;
         let event_type = event_json["type"].as_str().unwrap();
         Ok(match event_type {
             "PING" => Events::Ping,
             "MESSAGE_CREATED" => {
-                let channel_id = event_json["body"]["message"]["channelId"].as_str().ok_or("body.message.channelId is not string")?;
-                Events::MessageCreated {
-                    channel_id: channel_id.to_string(),
+                let channel_id = event_json["body"]["message"]["channelId"]
+                    .as_str()
+                    .ok_or("body.message.channelId is not string")?;
+                if event_json["body"]["message"]["embedded"]
+                    .as_array()
+                    .ok_or("body.message.embedded is not array")?
+                    .iter()
+                    .any(|emb| emb["id"].as_str() == Some(BOT_ID))
+                {
+                    let content = event_json["body"]["message"]["plainText"]
+                        .as_str()
+                        .ok_or("body.message.plainText is not string")?;
+                    Events::MentionMessageCreated {
+                        channel_id: channel_id.to_string(),
+                        content: content.to_string(),
+                    }
+                } else {
+                    Events::MessageCreated {
+                        channel_id: channel_id.to_string(),
+                    }
                 }
             }
             "DIRECT_MESSAGE_CREATED" => {
-                let channel_id = event_json["body"]["message"]["channelId"].as_str().ok_or("body.message.channelId is not string")?;
+                let channel_id = event_json["body"]["message"]["channelId"]
+                    .as_str()
+                    .ok_or("body.message.channelId is not string")?;
                 Events::DirectMessageCreated {
                     channel_id: channel_id.to_string(),
-                }
-            }
-            "MENTION_MESSAGE_CREATED" => {
-                let channel_id = event_json["body"]["message"]["channelId"].as_str().ok_or("body.message.channelId is not string")?;
-                let content = event_json["body"]["message"]["plainText"].as_str().ok_or("body.message.content is not string")?;
-                Events::MentionMessageCreated {
-                    channel_id: channel_id.to_string(),
-                    content: content.to_string(),
                 }
             }
             _ => return Err(format!("Unknown event type: {}", event_type)),
